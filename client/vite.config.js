3ae8a30646dfe9ecfa4a54821772fb7e790ff53c
Haoxuan Chen <henrychen743@gmail.com>
2025-11-26 10:14:04 -0500

    Update LMS frontend config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-const { PORT = 3000 } = process.env;
+// client/vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-      "/auth": {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-    },
-  },
-
+ 
   build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
+    outDir: 'dist',
   },
-});
+})
