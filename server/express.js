5a2544cb96a0eb513c04427446df6b307d54637c
Haoxuan Chen <henrychen743@gmail.com>
2025-11-26 08:52:29 -0500

    Relax CORS for deployed frontend

--- server/express.js
+++ server/express.js
@@ -15,10 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: true,      
+    credentials: true, 
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
